df94df7e5da616650fa19c05c5649eca54d9a17a
= <=>
2020-12-07 17:20:31 +0545

    Save New Record using Model

--- Django/MySite101/app8_1/urls.py
+++ Django/MySite101/app8_1/urls.py
@@ -3,4 +3,6 @@ from . import views # 1
 
 urlpatterns = [
     path('', views.index),
+    path('display_new_form', views.display_new_form),
+    path('save_new_person/', views.save_new_person),
 ]
--- Django/MySite101/app8_1/views.py
+++ Django/MySite101/app8_1/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Person
 
 # Create your views here.
@@ -8,3 +8,21 @@ def index(request):
     print(len(persons))
     context = {'persons': persons}
     return render(request, 'app8_1/index.html', context)
+
+def display_new_form(request):
+    return render(request, 'app8_1/new.html')
+
+def save_new_person(request):
+
+    # Receive values from Form
+    pid = request.POST.get('txt_id')
+    full_name  = request.POST.get('txt_name')
+    contact_address = request.POST.get('txt_address')
+
+    # Process for Save
+    person = Person() # Object of Model Class
+    person.pid = pid # Set Value
+    person.full_name=full_name
+    person.contact_address = contact_address
+    person.save()# Call Save Method
+    return HttpResponseRedirect("..")
